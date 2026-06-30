f2d4acea4ac68db8bca76197816b8f1c685117b5
novoice-rana <god0.x.trends@gmail.com>
2026-06-30 14:50:28 +0530

    fixed everything

---
+++ packages/db/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.prismaClient = void 0;
+const client_1 = require("@prisma/client");
+const adapter_pg_1 = require("@prisma/adapter-pg");
+const adapter = new adapter_pg_1.PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
+exports.prismaClient = new client_1.PrismaClient({ adapter, });
